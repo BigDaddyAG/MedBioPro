d602cda10d877e7bb2cf75e35abdf54b42ce9354
stfn-jcb <s.jacob@fu-berlin.de>
2015-09-27 16:42:43 +0200

    Update code

--- src/main/scala/de/BigDaddyAG/BcrSmokerPrediction.scala
+++ src/main/scala/de/BigDaddyAG/BcrSmokerPrediction.scala
@@ -20,8 +20,8 @@ package de.BigDaddyAG
 import org.apache.flink.api.scala._
 
 
-case class ConsentStatus(bcrPatientBarcode: String, patientConsentStatus: String)
-case class smokeStatus(bcrPatientBarcode: String, patientConsentStatus: String)
+case class ConsentStatus(consentPatientBarcode: String, patientConsentStatus: String)
+case class smokerStatus(smokerPatientBarcode: String, patientSmokerStatus: String)
 
 
 
@@ -30,38 +30,32 @@ object BcrSmokerPrediction {
   def main(args: Array[String]) {
 
     val consentStatusFile = "/Users/stefan/Documents/Uni/SoSe 2015/Medical Bioinformatics/assignment11/BigDaddyAG/MedBioPro/data/BCR/Clinical/Biotab/nationwidechildrens.org_biospecimen_cqcf_luad.txt"
-    val smokeStatusFile = "/Users/stefan/Documents/Uni/SoSe 2015/Medical Bioinformatics/assignment11/BigDaddyAG/MedBioPro/data/BCR/Clinical/Biotab/nationwidechildrens.org_clinical_patient_luad.txt"
+    val smokerStatusFile = "/Users/stefan/Documents/Uni/SoSe 2015/Medical Bioinformatics/assignment11/BigDaddyAG/MedBioPro/data/BCR/Clinical/Biotab/nationwidechildrens.org_clinical_patient_luad.txt"
 
     // enable recursive enumeration of nested input files
     val env = ExecutionEnvironment.getExecutionEnvironment
 
-/*    val smokeStatusData = env.readCsvFile[smokeStatus] (
-      consentStatusFile,
-      fieldDelimiter = ",",
-      includedFields = Array(1, 45)
-    )*/
 
-    val gccFile =
-      readConsentStatusData(env, consentStatusFile, Array(1,3))
+    val consentStatusData =
+      readConsentStatusData(env, consentStatusFile, Array(1, 3))
         .as('bcrPatientStatus, 'patientConsentStatus)
 
+    val smokerStatusData =
+      readConsentSmokerData(env, smokerStatusFile, Array(1, 45))
+        .as('bcrPatientStatus, 'patientConsentStatus)
 
     env.execute("Make it run!!1!")
 
   }
 
-  // This method reads all rows but only selected columns from a file and returns a dataset
-  private def readConsentStatusData(env: ExecutionEnvironment, path: String, fieldDelimiter: String, includedFields: String): DataSet[ConsentStatus] = {
-    env.readCsvFile[gccColumnItems] (
-      path,
-      fieldDelimiter,
-      includedFields )
-  }
 
   private def readConsentStatusData(env: ExecutionEnvironment, path: String, includedCols: Array[Int]): DataSet[ConsentStatus] = {
     env.readCsvFile[ConsentStatus](path, fieldDelimiter = ",", includedFields = includedCols)
   }
 
+  private def readSmokerStatusData(env: ExecutionEnvironment, path: String, includedCols: Array[Int]): DataSet[ConsentStatus] = {
+    env.readCsvFile[ConsentStatus](path, fieldDelimiter = ",", includedFields = includedCols)
+  }
 
 
 }
